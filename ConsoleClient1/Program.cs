adb74a23b1127b1845242009d20fa562a0ee4199
Евгений Картавых <evgeniy@kartavykh.com>
2024-10-04 13:09:26 +0300

    Add project files.

---
+++ ConsoleClient1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace ConsoleClient1
+{
+    internal class Program
+    {
+        public enum operation
+        {
+            plus,
+            minus,
+            mult,
+            div
+        }
+
+        class request
+        {
+            public double v1 { get; set; }
+            public double v2 { get; set; }
+            public operation op { get; set; }
+        }
+        class response
+        {
+            public double result { get; set; }
+            public string message { get; set; }
+            public bool isError => !String.IsNullOrEmpty(message);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value 1: ");
+            double v1i, v2i;
+            if (!Double.TryParse(Console.ReadLine(), out v1i))
+            {
+                Console.WriteLine("Wrong number format!!!");
+                return;
+            }
+            Console.Write("Enter value 2: ");
+            if (!Double.TryParse(Console.ReadLine(), out v2i))
+            {
+                Console.WriteLine("Wrong number format!!!");
+                return;
+            }
+            Console.Write("Enter operation: ");
+            var operationS = Console.ReadLine();
+            operation opi = operation.plus;
+            switch (operationS)
+            {
+                case "+": opi = operation.plus; break;
+                case "-": opi = operation.minus; break;
+                case "/": opi = operation.div; break;
+                case "*": opi = operation.mult; break;
+                default: Console.WriteLine($"Wrong operation: {operationS}"); return;
+            }
+
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect("127.0.0.1", 10001);
+            using (var stream = tcpClient.GetStream())
+            {
+                // формируем запрос на сервере
+                request r = new request() { v1 = v1i, v2 = v2i, op = opi };
+                var rString = JsonSerializer.Serialize(r);
+                var bytes = Encoding.UTF8.GetBytes(rString);
+
+                stream.Write(bytes, 0, bytes.Length); // отправляем запрос
+                stream.Flush();
+                // читаем ответ сервера
+                byte[] buf = new byte[512];
+                List<byte> inBytes = new List<byte>();
+                do
+                {
+                    int c = stream.Read(buf, 0, buf.Length);
+                    inBytes.AddRange(buf.Take(c));
+                }
+                while (stream.DataAvailable);
+                // разбираем что нам прислал сервер
+                var resp = JsonSerializer.Deserialize<response>(Encoding.UTF8.GetString(inBytes.ToArray()));
+
+                // выводим ответ
+                Console.WriteLine($"Answer is: {resp.result}");
+            }
+            tcpClient.Close();
+        }
+    }
+}
---
+++ ConsoleServerApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace ConsoleServerApp
+{
+    internal class Program
+    {
+        public enum operation
+        {
+            plus,
+            minus,
+            mult,
+            div
+        }
+        class request
+        {
+            public double v1 { get; set; }
+            public double v2 { get; set; }
+            public operation op { get; set; }
+        }
+        class response
+        {
+            public double result { get; set; }
+            public string message { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 10001);
+            listener.Start();
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+
+                using (var stream = client.GetStream())
+                {
+                    byte[] buf = new byte[10];
+                    List<byte> bytes = new List<byte>();
+                    do
+                    {
+                        int c = stream.Read(buf, 0, buf.Length);
+                        bytes.AddRange(buf.Take(c));
+                    }
+                    while (stream.DataAvailable);
+                    var rString = Encoding.UTF8.GetString(bytes.ToArray());
+                    var r = JsonSerializer.Deserialize<request>(rString);
+                    response response = new response();
+                    if (r.op == operation.div && r.v2 == 0)
+                    {
+                        response.message = "Делить на 0 нельзя!";
+                    }
+                    else
+                    {
+                        string opS = "";
+                        double result = 0;
+                        switch (r.op)
+                        {
+                            case operation.plus: result = (r.v1 + r.v2); opS = "+"; break;
+                            case operation.minus: result = (r.v1 - r.v2); opS = "-"; break;
+                            case operation.mult: result = (r.v1 * r.v2); opS = "*"; break;
+                            case operation.div: result = (r.v1 / r.v2); opS = "/"; break;
+                        }
+                        Console.WriteLine($"{r.v1}{opS}{r.v2} = {result}");
+                        response.result = result;
+                    }
+                    var data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+                    stream.Write(data, 0, data.Length);
+                    //Console.WriteLine("Recieved message: " + Encoding.UTF8.GetString(bytes.ToArray()));
+                }
+
+            }
+        }
+    }
+}
